2cc002fc9ffa4dfd1b934be39f37c2cecd427581
Willy Mello <cwmello165@gmail.com>
2019-03-25 11:55:19 -0400

    added main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const { db } = require('./server/db')
+const app = require('./server')
+const PORT = 1337
+
+db.sync()
+  .then(() => {
+    console.log('db synced')
+    app.listen(PORT, () => console.log(`studiously serving silly sounds on port ${PORT}`))
+  })
