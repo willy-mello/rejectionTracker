4b62861820c1604d031b9de8f4c3ecb2cddfcf3d
Willy Mello <cwmello165@gmail.com>
2019-03-26 13:16:27 -0400

    update is working

--- server/api/applications.js
+++ server/api/applications.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-// const Students = require('../db/student')
+
 const { Application } = require('../db')
 
 
@@ -54,10 +54,26 @@ router.post('/', async (req, res, next) => {
 //     next(error)
 //   }
 // })
-// router.use((req, res, next) => {
-//   const err = new Error('Application route not found!')
-//   err.status = 404
-//   next(err)
-// })
+router.put('/:id', async (req, res, next) => {
+  console.log('req.params.id', req.params.id)
+  try {
+    const app = await Application.update({
+      rejected: true
+    }, {
+        where: { id: req.params.id },
+        returning: true, // needed for affectedRows to be populated
+        plain: true // makes sure that the returned instances are just plain objects
+      })
+    res.json(app)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.use((req, res, next) => {
+  const err = new Error('Application route not found!')
+  err.status = 404
+  next(err)
+})
 
 module.exports = router
